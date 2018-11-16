1c839c916c2d0a08e0ce7d8255c491999672cd1f
Yanan Gao <gao.yana@husky.neu.edu>
2018-11-16 00:37:45 -0800

    add query ports

---
+++ src/dao/dealerQuery.java
@@ -0,0 +1,23 @@
+package dao;
+
+import main.Components.Dealer;
+
+import java.util.*;
+
+public class dealerQuery {
+
+
+
+    public Dealer findDealerByID(String id){
+
+    }
+    public List<Dealer> findDealersByCity(String city){
+
+    }
+    public List<Dealer> findDealersByPostCode(int postCode){
+
+    }
+
+
+
+}
---
+++ src/dao/vehicleQuery.java
@@ -0,0 +1,23 @@
+package dao;
+
+import main.Components.BodyType;
+import main.Components.Vehicle;
+import java.util.*;
+
+public class vehicleQuery {
+
+
+    public Vehicle getVehicleByID(String id){
+
+    }
+    public List<String> getVehicleIDsByDealerID(String dealerID){
+
+    }
+    public List<String> getVehicleIDsByCity(String city){
+
+    }
+    public List<String> getVehicleIDsByType(BodyType type){
+
+    }
+
+}
