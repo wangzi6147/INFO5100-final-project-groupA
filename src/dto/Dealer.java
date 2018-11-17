ecd873181639e48d1eef4473d27494a2dd45974f
Yanan Gao <gao.yana@husky.neu.edu>
2018-11-16 22:37:45 -0800

    modified dealer address class

--- src/dto/Dealer.java
+++ src/dto/Dealer.java
@@ -5,11 +5,8 @@ import java.io.Serializable;
 public class Dealer implements Serializable {
     private String id;
     private String name;
-    private String address1;
-    private String address2;
-    private String city;
-    private String state;
-    private String zip;
+    private Address address;
+
 
     public Dealer(String name) {
         this.name = name;
@@ -30,46 +27,29 @@ public class Dealer implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public void setAddress(Address add){  this.address = add; }
+
+    public Address getAddress(){ return this.address; }
 
     public String getAddress1() {
-        return address1;
-    }
-
-    public void setAddress1(String address1) {
-        this.address1 = address1;
+        return this.address.getAddress1();
     }
 
     public String getAddress2() {
-        return address2;
-    }
-
-    public void setAddress2(String address2) {
-        this.address2 = address2;
+        return this.address.getAddress2();
     }
 
     public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
+        return this.address.getCity();
     }
 
     public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
+        return this.address.getState();
     }
 
     public String getZip() {
-        return zip;
+        return this.address.getZip();
     }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
 
 }
