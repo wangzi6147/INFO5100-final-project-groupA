f71ef3dbbd397296b64817b22a56d646be90872c
Wenchao Ma <wenchaoma@wenchaos-mbp.lan>
2018-11-18 16:11:15 -0800

    ui

---
+++ src/ui/vehicle/VehicleSearchMainView.java
@@ -0,0 +1,13 @@
+package ui.vehicle;
+
+public class VehicleSearchMainView {
+
+    public VehicleSearchMainView(){
+        initializeMainView();
+    }
+
+    private void initializeMainView() {
+
+    }
+
+}
