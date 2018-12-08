732635f668b7470dea9deabee0325970190eaf88
KKKlaus <klauslin666@gmail.com>
2018-12-08 14:18:32 -0800

    add interfaces

--- src/service/SpecialService.java
+++ src/service/SpecialService.java
@@ -1,10 +1,21 @@
 package service;
 
+import java.text.ParseException;
 import java.util.List;
 
 import dto.Special;
 import dto.Vehicle;
 
 public interface SpecialService {
-	List<Vehicle> addSpecial(Special special, List<Vehicle> vehicles);
+	//add special to the vehicles that match it, return the number of vehicles match this adding special
+	int addSpecial(Special special, List<Vehicle> vehicles);
+	
+	//delete special to the vehicles that match it, return the number of vehicles match this deleting special
+	int deleteSpecial(Special special, List<Vehicle> vehicles);
+	
+	//search the special by id in database
+	Special getSpecialBySpecialID(String id);
+	
+	//associate a list of Specials to a list of vehicles, and then return assigned vehicles
+	List<Vehicle> associateSpecials(List<Vehicle> vehicles, List<Special> specials) throws ParseException;
 }
