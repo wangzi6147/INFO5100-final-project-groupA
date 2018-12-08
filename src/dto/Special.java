824a27128faca0cbed5f234083b5239d7bac9349
KKKlaus <klauslin666@gmail.com>
2018-12-08 14:52:33 -0800

    a

--- src/dto/Special.java
+++ src/dto/Special.java
@@ -32,7 +32,7 @@ public class Special {
     private ValueType valueType;
     
     
-    
+    //
     public Special(String id, String dealerID, String startDate, String endDate, String title, String brand, String year, boolean isNew,
                    BodyType bodyType, String value, ValueType valueType) {
     	
